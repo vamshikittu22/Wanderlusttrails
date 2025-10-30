26cdd1aa497e3b98ef9ce27d2a7004e59558eb95
vamshikittu22 <krishnavamshi.2297@gmail.com>
2025-10-30 01:32:56 -0500

    package image name

--- Backend/config/AdminDashboard/manageDestinations/editPackage.php
+++ Backend/config/AdminDashboard/manageDestinations/editPackage.php
@@ -66,7 +66,7 @@ if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
     }
 
     // Generate unique filename to prevent collisions
-    $imageName = time() . '_' . basename($image['name']);
+    $imageName = time() . '_' . $packageName . '.jpg';
     $uploadPath = $uploadDir . $imageName;
 
     // Move uploaded file to destination folder
